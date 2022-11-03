1d5a46f7ba2864c6855888d13c347483a85a7c1c
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-11-03 16:36:53 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/com/raisetech/mybatisdemob/MybatisdemobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.mybatisdemob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisdemobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
